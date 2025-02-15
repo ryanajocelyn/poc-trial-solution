f587c0b45125bb13234a4fc0ec50d6f99fb8b3bd
ryanajocelyn <abijeet.darwin@gmail.com>
2025-02-15 21:36:28 +0530

    mygate - enhancements

--- src/account/main.py
+++ src/account/main.py
@@ -190,10 +190,10 @@ class Maintenance(Base):
 if __name__ == "__main__":
     # Start Row - Row to start from
     m_params = {
-        "base_path": "D:\\Abiz\\Flats\\Vedanshi\\2024-25\\10. Jan",
-        "stmt_nm": "DetailedStatement-3.pdf",
-        "tpl_nm": "batch_dues_receipt_upload_5724207_.csv",
-        "start_row": 34,
+        "base_path": "D:\\Abiz\\Flats\\Vedanshi\\2024-25\\11. Feb",
+        "stmt_nm": "DetailedStatement-1.pdf",
+        "tpl_nm": "batch_dues_receipt_upload_5861508_.csv",
+        "start_row": 0,
         "ner": False,
     }
     maint = Maintenance(params=m_params)
